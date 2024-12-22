709167ee7840175a14d942cc2d9fbecf4f2b408d
seungki <sysh9498@gmail.com>
2024-12-22 21:03:00 +0900

    feat: 전체 Frame 컴포넌트 구현

---
+++ frontend/src/components/Frame.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const Frame = styled.div`
+  width: 390px;
+  /* height: calc(var(--vh, 1vh) * 100); */
+  height: 100vh;
+  padding: 0 24px;
+  background-color: #F9FAFC;
+  position: relative;
+`;
+
+export { Frame };
