e0dec5cc68f1d08342797e75e3726da62fa5c52d
seungki <sysh9498@gmail.com>
2024-12-23 23:26:16 +0900

    fix: 편지지 이름 변경

--- frontend/src/type/LetterPaperProps.ts
+++ frontend/src/type/LetterPaperProps.ts
@@ -1,7 +1,11 @@
-const LetterPaperProps = {
-  GRAPH_PAPER: {
-    imgUrl: "/img/letterPapers/letterPaper_2.svg",
+const LetterPaperProps = [
+  {
+    keyName: "GRAPH_PAPER",
+    imgUrl: "/img/letterPapers/graph-paper.svg",
+    name: "모눈종이",
   },
-};
+  { keyName: "BASIC_NOTE", imgUrl: "/img/letterPapers/basic-note.svg", name: "유선노트" },
+  { keyName: "POLAROID", imgUrl: "/img/letterPapers/polaroid.svg", name: "폴라로이드" },
+];
 
 export { LetterPaperProps };
