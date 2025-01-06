295104a710bdd3754c501a2c1b18f0b82f794394
seungki <sysh9498@gmail.com>
2025-01-06 22:08:15 +0900

    fix: authorization header 수정

--- frontend/src/auth/api.ts
+++ frontend/src/auth/api.ts
@@ -4,9 +4,8 @@ const api = axios.create({
   baseURL: process.env.REACT_APP_BASE_URL,
 });
 
-const accessToken = sessionStorage.getItem("accessToken");
 
 // 기본 Authorization 헤더 설정
-api.defaults.headers.common["Authorization"] = `Bearer ${accessToken}`;
+api.defaults.headers.common["Authorization"] = `Bearer ${sessionStorage.getItem("accessToken")}`;
 
 export default api;
