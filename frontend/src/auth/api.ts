1706b9fb196fe2ce26aada6d924f553df8b368d2
seungki <sysh9498@gmail.com>
2025-01-06 20:39:20 +0900

    feat: axios api 설정 추가

--- frontend/src/auth/api.ts
+++ frontend/src/auth/api.ts
@@ -1,1 +1,12 @@
 import axios from "axios";
+
+const api = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+});
+
+const accessToken = sessionStorage.getItem('accessToken');
+
+// 기본 Authorization 헤더 설정
+axios.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
+
+export default api;
