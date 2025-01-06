5df0cd3c171a9ebf09c474fe5beca6ae0dbbe493
seungki <sysh9498@gmail.com>
2025-01-06 20:40:49 +0900

    feat: Frame 위치 이동

--- frontend/src/stores/useNaverLogin.ts
+++ frontend/src/stores/useNaverLogin.ts
@@ -1,4 +1,5 @@
 import axios, { AxiosError } from "axios";
+import { useNavigate } from "react-router";
 import { create } from "zustand";
 
 const NAVER_CLIENT_ID = process.env.REACT_APP_NAVER_CLIENT_ID; // 발급받은 클라이언트 아이디
@@ -10,7 +11,7 @@ interface NaverLoginProps {
   isLoading: boolean;
   error: AxiosError | null;
   startLogin: () => void;
-  sendAuthCode: (authCode: string) => void;
+  sendAuthCode: (authCode: string, state: string, navigate: (path: string)=>void) => void;
 }
 
 const useNaverLogin = create<NaverLoginProps>(set => ({
@@ -19,11 +20,28 @@ const useNaverLogin = create<NaverLoginProps>(set => ({
   startLogin() {
     window.location.href = NAVER_AUTH_URL;
   },
-  sendAuthCode: async authCode => {
+  sendAuthCode: async (authCode, state, navigate) => {
     set({ isLoading: true });
     axios
-      .post(process.env.REACT_APP_BASE_URL + `/login/naver/${authCode}`)
-      .then(res => console.log(res))
+      .post(process.env.REACT_APP_BASE_URL + `/auth/naver`, {
+        code: authCode,
+        state: state.toString()
+      })
+      .then(res => 
+      { 
+        console.log(res);
+        const {isFirstVisit, userId, token} = res.data.data;
+        const {accessToken, accessTokenExpiredTime, refreshToken} = token;
+        sessionStorage.setItem("accessToken", accessToken);
+        sessionStorage.setItem("atExpiredTime", accessTokenExpiredTime);
+        localStorage.setItem("refreshToken", refreshToken);
+        if (isFirstVisit) {
+          navigate("/makePostBox")
+        } else {
+          navigate(`/postbox/${userId}`)
+        }
+      }
+      )
       .catch(error => set({ error }))
       .finally(() => set({ isLoading: false }));
   },
