b20b8d93c05724d25b138ddd1bcbb91c4fcd03c1
seungki <sysh9498@gmail.com>
2025-01-09 20:15:44 +0900

    fix: height 100vh로 변경

--- frontend/src/layouts/Frame.ts
+++ frontend/src/layouts/Frame.ts
@@ -2,8 +2,8 @@ import styled from "styled-components";
 
 const Frame = styled.div`
   width: 100vw;
-  height: calc(var(--vh, 1vh) * 100);
-  /* height: 100vh; */
+  /* height: calc(var(--vh, 1vh) * 100); */
+  height: 100vh;
   padding: 0 24px;
   background-color: #F9FAFC;
   position: relative;
