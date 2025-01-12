093dc720566b5c4adfbf9522eeaa570e805432d9
seungki <sysh9498@gmail.com>
2025-01-12 17:22:42 +0900

    feat: 편지지 별 폰트 옵션 설정

--- frontend/src/type/LetterFontProps.ts
+++ frontend/src/type/LetterFontProps.ts
@@ -1,32 +1,66 @@
+export type LetterPaperType = "GRAPH_PAPER" | "BASIC_NOTE" | "PHOTO_POSTCARD" | "POLAROID" | "CHECK_PAPER";
+
 const LetterFontProps = {
   Pretendard_R: {
     "font-family": "Pretendard-R",
-    "font-size": "14.5px",
+    "font-size": {
+      GRAPH_PAPER: "14.5px",
+      BASIC_NOTE: "14.5px",
+      PHOTO_POSTCARD: "13px",
+      POLAROID: "15.5px",
+      CHECK_PAPER: "14.5px",
+    },
   },
   Gmarket_R: {
     "font-family": "Gmarket-R",
-    "font-size": "14px",
+    "font-size": {
+      GRAPH_PAPER: "13.5px",
+      BASIC_NOTE: "13.5px",
+      PHOTO_POSTCARD: "12px",
+      POLAROID: "14px",
+      CHECK_PAPER: "13.5px",
+    },
   },
   Leeseoyun: {
     "font-family": "Leeseoyun",
-    "font-size": "17px",
+    "font-size": {
+      GRAPH_PAPER: "17px",
+      BASIC_NOTE: "17px",
+      PHOTO_POSTCARD: "14.5px",
+      POLAROID: "18px",
+      CHECK_PAPER: "17px",
+    },
   },
   Gowun_R: {
     "font-family": "Gowun-R",
-    "font-size": "14.5px",
+    "font-size": {
+      GRAPH_PAPER: "14.5px",
+      BASIC_NOTE: "14.5px",
+      PHOTO_POSTCARD: "12.5px",
+      POLAROID: "16px",
+      CHECK_PAPER: "14.5px",
+    },
   },
   KCC: {
     "font-family": "KCC",
-    "font-size": "23.6px",
-  },
-  Kangwon: {
-    "font-family": "Kangwon",
-    "font-size": "16px",
+    "font-size": {
+      GRAPH_PAPER: "22px",
+      BASIC_NOTE: "22px",
+      PHOTO_POSTCARD: "18px",
+      POLAROID: "22px",
+      CHECK_PAPER: "22px",
+    },
   },
   HiMelody_R: {
     "font-family": "HiMelody-R",
-    "font-size": "18px"
-  }
+    "font-size": {
+      GRAPH_PAPER: "18px",
+      BASIC_NOTE: "18px",
+      PHOTO_POSTCARD: "14.5px",
+      POLAROID: "20px",
+      CHECK_PAPER: "17.5px",
+    },
+  },
 };
 
 export { LetterFontProps };
