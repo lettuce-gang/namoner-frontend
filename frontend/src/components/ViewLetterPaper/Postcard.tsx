9cff7dfbf4015efe7b5feac09a3afdd9489b2f96
seungki <sysh9498@gmail.com>
2025-01-16 23:10:29 +0900

    feat: 포토엽서 뷰 추가

---
+++ frontend/src/components/ViewLetterPaper/Postcard.tsx
@@ -0,0 +1,68 @@
+import React from "react";
+import styled from "styled-components";
+import { LetterFontProps } from "../../type/LetterFontProps.ts";
+import { LetterProps } from "../../stores/useLetter.ts";
+
+type LetterInfoType = {
+  letter: LetterProps;
+};
+
+type LetterDivType = {
+  fontSize: string;
+  fontFamily: string;
+};
+
+function Postcard({ letter }: LetterInfoType) {
+  return (
+    <Wrapper>
+      <LetterPaper src="/img/writeLetterPaper/postcard-write-letter-paper.svg" />
+      {letter.imageUrl && <LetterImg src={letter.imageUrl} alt="편지 이미지" />}
+      <LetterText
+        fontFamily={LetterFontProps[letter.fontType]["font-family"]}
+        fontSize={LetterFontProps[letter.fontType]["font-size"].PHOTO_POSTCARD}
+      >
+        {letter.message}
+      </LetterText>
+    </Wrapper>
+  );
+}
+
+export default Postcard;
+
+const Wrapper = styled.div`
+  position: relative;
+  width: 100%;
+`;
+
+const LetterPaper = styled.img`
+  width: 100%;
+  height: 220px;
+`;
+
+const LetterImg = styled.img`
+  position: absolute;
+  top: 18px;
+  left: 17px;
+  width: 122px;
+  height: 164px;
+  object-fit: fill;
+  z-index: 10;
+`;
+
+const LetterText = styled.div<LetterDivType>`
+  position: absolute;
+  right: 15px;
+  top: 6px;
+  width: 140px;
+  height: 205px;
+  line-height: 24px;
+  box-sizing: border-box;
+  background-color: transparent;
+  border-radius: 5px;
+  padding: 5px;
+  overflow: hidden;
+  word-wrap: break-word;
+  white-space: pre-wrap;
+  font-size: ${props => props.fontSize || "14.5px"};
+  font-family: ${props => props.fontFamily || "Pretendard-R"};
+`;
