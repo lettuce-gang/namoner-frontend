eb55b6f7139a8111459f6c71b86d93f030c97e6f
seungki <sysh9498@gmail.com>
2025-01-19 23:38:09 +0900

    feat: 편지 없을 때 화면 구현

--- frontend/src/components/ReceivedMailContainer.tsx
+++ frontend/src/components/ReceivedMailContainer.tsx
@@ -4,14 +4,25 @@ import styled from "styled-components";
 import NormalLetterEnvelope from "./NormalLetterEnvelope.tsx";
 import ReservedLetterEnvelope from "./ReservedLetterEnvelope.tsx";
 
-
 interface ReceivedMailContainerProp {
-    letters: LetterListProps[]
+  letters: LetterListProps[];
 }
 
-function ReceivedMailContainer({letters}: ReceivedMailContainerProp) {
+function ReceivedMailContainer({ letters }: ReceivedMailContainerProp) {
+  const isEmpty = letters.length === 0;
   return (
     <MailListContainer>
+      {isEmpty && (
+        <EmptyLetterContainer>
+          <img src="/img/no-letter-img.svg" alt="empty-letter" width={66} height={66} />
+          <p>우체통이 텅 비었어요!</p>
+          <span>
+            도착한 편지가 없어요
+            <br />
+            가장 먼저 편지를 써서 마음을 전달해보세요!
+          </span>
+        </EmptyLetterContainer>
+      )}
       {letters &&
         letters.map(letter => {
           if (letter.letterType === "NORMAL") {
@@ -39,5 +50,34 @@ const MailListContainer = styled.div`
   overflow: auto;
   > * {
     flex-shrink: 0;
+  }
+`;
+
+const EmptyLetterContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  width: 100%;
+  height: 100%;
+  p {
+    color: #262626;
+    text-align: center;
+    font-family: "Pretendard-B";
+    font-size: 20px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+  }
+  span {
+    color: #777;
+    text-align: center;
+    font-family: "Pretendard-M";
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: 21px; /* 131.25% */
+    letter-spacing: -0.24px;
   }
 `;
