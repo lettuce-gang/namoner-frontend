9371fcb2bdbc68be7fb73078c04b988efe98072b
seungki <sysh9498@gmail.com>
2025-03-06 11:13:14 +0900

    fix: 훅 변경

--- frontend/src/auth/api.ts
+++ frontend/src/auth/api.ts
@@ -1,4 +1,6 @@
 import axios from "axios";
+import { useStore } from "zustand";
+import { useUserInfo } from "../stores/useUserInfo.ts";
 
 const api = axios.create({
   baseURL: process.env.REACT_APP_BASE_URL,
@@ -11,28 +13,33 @@ const isTokenExpired = () => {
   return new Date().getTime() > new Date(expiredTime).getTime();
 };
 
-// 토큰 리프레시 함수
-const refreshAccessToken = async () => {
-  try {
-    const _refreshToken = localStorage.getItem("refreshToken");
-    const response = await axios.post(`${process.env.REACT_APP_BASE_URL}/auth/reissue`, {
-      refreshToken: _refreshToken,
-    });
-    console.log("responseRefresh", response);
-    const { accessToken, refreshToken, accessTokenExpiredTime } = response.data.token;
-    sessionStorage.setItem("accessToken", accessToken);
-    sessionStorage.setItem("atExpiredTime", accessTokenExpiredTime);
-    localStorage.setItem("refreshToken", refreshToken);
+// 사용자 정의 훅으로 변경
+const useRefreshAccessToken = () => {
+  const { logout } = useStore(useUserInfo);
 
-    return accessToken;
-  } catch (error) {
-    // 리프레시 토큰도 만료된 경우
-    sessionStorage.clear();
-    localStorage.clear();
-    alert("로그인 토큰이 만료되었습니다. 다시 로그인해주세요.");
-    window.location.href = "/signup"; // 로그인 페이지로 리다이렉트
-    return null;
-  }
+  const refreshAccessToken = async () => {
+    try {
+      const _refreshToken = localStorage.getItem("refreshToken");
+      const response = await axios.post(`${process.env.REACT_APP_BASE_URL}/auth/reissue`, {
+        refreshToken: _refreshToken,
+      });
+      console.log("responseRefresh", response);
+      const { accessToken, refreshToken, accessTokenExpiredTime } = response.data.token;
+      sessionStorage.setItem("accessToken", accessToken);
+      sessionStorage.setItem("atExpiredTime", accessTokenExpiredTime);
+      localStorage.setItem("refreshToken", refreshToken);
+
+      return accessToken;
+    } catch (error) {
+      // 리프레시 토큰도 만료된 경우
+      logout();
+      alert("로그인 토큰이 만료되었습니다. 다시 로그인해주세요.");
+      window.location.href = "/signup"; // 로그인 페이지로 리다이렉트
+      return null;
+    }
+  };
+
+  return refreshAccessToken;
 };
 
 // Request Interceptor
@@ -58,6 +65,7 @@ api.interceptors.response.use(
     const originalRequest = error.config;
     if (error.response.status === 401 && !originalRequest._retry) {
       originalRequest._retry = true; // 재시도 플래그 설정
+      const refreshAccessToken = useRefreshAccessToken(); // 사용자 정의 훅 호출
       const newToken = await refreshAccessToken(); // 리프레쉬 토큰으로 엑세스 토큰 재발급
       if (newToken) {
         originalRequest.headers.Authorization = `Bearer ${newToken}`; // 새로운 토큰으로 헤더 업데이트
