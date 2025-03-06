015124ea278355f6aa7390fa8f9c40a10c1de0b0
seungki <sysh9498@gmail.com>
2025-03-06 11:42:27 +0900

    feat: 이름 변경 아이콘 변경

--- frontend/src/components/SideTab.tsx
+++ frontend/src/components/SideTab.tsx
@@ -47,9 +47,7 @@ function SideTab({ isOpen, onClose, isLoggedIn }: SideTabProps) {
         ) : (
           <>
             {postBoxName}
-            <span onClick={handleEditClick} style={{ cursor: "pointer" }}>
-              ✍️
-            </span>
+            <img src="/img/icon-edit.svg" width={16} onClick={handleEditClick} style={{ cursor: "pointer", marginLeft: 5 }} />
           </>
         )}
       </MenuTitle>
@@ -212,6 +210,6 @@ const Span_ = styled.span`
 const FlexBox = styled.div`
   display: flex;
   align-items: center;
-`
+`;
 
 export default SideTab;
